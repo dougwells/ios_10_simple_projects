14f9d9a7b6f7a9cd7640f97b7467275958b52577
dougwells <dougwells@gmail.com>
2016-11-10 12:56:47 -0700

    iOS. Simple Apps. Calculator Jr. Connect operator buttons to ViewController

--- simpleKidCalculator/Calc Jr/Calc Jr/ViewController.swift
+++ simpleKidCalculator/Calc Jr/Calc Jr/ViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //button Actions
+    @IBAction func numberClicked (_ sender: UIButton) {
+
+    }
+    
+    @IBAction func operatorClicked (_ sender: UIButton) {
+        switch sender.tag {
+        case 10:
+            print("plus")
+        case 11:
+            print("minus")
+        case 12:
+            print ("times")
+        case 13:
+            print("divide")
+        default:
+            return
+        }
+    }
+    
+    @IBAction func equalsClicked (_ sender: UIButton) {
+        
     }
 
     override func didReceiveMemoryWarning() {
