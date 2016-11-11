ac152513e6891b012c0794168d483e157b90b8ff
dougwells <dougwells@gmail.com>
2016-11-11 07:32:46 -0700

    iOS. Simple Apps. Calculator Jr. Add calculator logic.

--- simpleKidCalculator/Calc Jr/Calc Jr/ViewController.swift
+++ simpleKidCalculator/Calc Jr/Calc Jr/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
     var currentOperation: Operator = Operator.nothing
     var calcState: CalculationState = CalculationState.enteringNum
+    
+    var firstValue: String = ""
 
     @IBOutlet weak var resultLabel: UILabel!
     
@@ -21,27 +23,70 @@ class ViewController: UIViewController {
     
     //button Actions
     @IBAction func numberClicked (_ sender: UIButton) {
-
+        updateDisplay(number: String(sender.tag))
+    }
+    
+    func updateDisplay (number: String) {
+        if calcState == CalculationState.newNumStarted {
+            if let num = resultLabel.text {
+                if num != ""{
+                    firstValue = num
+                }
+            }
+            calcState = CalculationState.enteringNum
+            resultLabel.text = number
+        } else if calcState == CalculationState.enteringNum {
+            resultLabel.text = resultLabel.text! + number   //concat
+        }
     }
     
     @IBAction func operatorClicked (_ sender: UIButton) {
+        calcState = CalculationState.newNumStarted
+        print(firstValue)
+        if let num = resultLabel.text {
+            if num != "" {
+                firstValue = num        //I think this line is not needed
+                resultLabel.text = ""
+            }
+        }
+        
         switch sender.tag {
         case 10:
-            print("plus")
+            currentOperation = Operator.add
         case 11:
-            print("minus")
+            currentOperation = Operator.subtract
         case 12:
-            print ("times")
+            currentOperation = Operator.times
         case 13:
-            print("divide")
+            currentOperation = Operator.divide
         default:
             return
         }
     }
     
     @IBAction func equalsClicked (_ sender: UIButton) {
-        
+        calcSum()
     }
+    
+        func calcSum(){
+            if firstValue.isEmpty {
+                return
+            }
+            
+            var result = ""
+            
+            if (currentOperation == Operator.times) {
+                result = String(Double(firstValue)! * Double(resultLabel.text!)!)
+            } else if (currentOperation == Operator.divide) {
+                result = String(Double(firstValue)! / Double(resultLabel.text!)!)
+            } else if (currentOperation == Operator.add) {
+                result = String(Double(firstValue)! + Double(resultLabel.text!)!)
+            } else if (currentOperation == Operator.subtract) {
+                result = String(Double(firstValue)! - Double(resultLabel.text!)!)
+            }
+            resultLabel.text = result
+            calcState = CalculationState.newNumStarted
+        }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
