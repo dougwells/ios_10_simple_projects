69ee1fd29647f80eb60eabe94a807c33cebe07de
dougwells <dougwells@gmail.com>
2016-11-12 10:50:21 -0700

    iOS. Simple Apps. Dog Years.  Calculate age of dog in human years. Init & Final Commit

---
+++ simpleDogYears/dogYears/dogYears/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  dogYears
+//
+//  Created by Doug Wells on 11/12/16.
+//  Copyright © 2016 Doug Wells. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var enterAge: UITextField!
+    @IBOutlet weak var ansPrefixLabel: UILabel!
+    @IBOutlet weak var ansPostLabel: UILabel!
+
+    @IBAction func calcAgeBtnTapped(_ sender: Any) {
+        if (enterAge.text != "") {
+            let age = Double(enterAge.text!)! * 7
+            ansPrefixLabel.text = "In dog years, your dog is: "
+            ansPostLabel.text = String(age)
+        }
+    }
+
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
