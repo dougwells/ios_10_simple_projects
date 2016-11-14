0b67f6a6e262aec9ce63a1aa3afd3a5b7f8de02f
dougwells <dougwells@gmail.com>
2016-11-13 17:37:38 -0700

    iOS. Simple Apps. Is it Prime? Finished logic.  Final Commit

--- Is It Prime/Is It Prime/ViewController.swift
+++ Is It Prime/Is It Prime/ViewController.swift
@@ -17,19 +17,24 @@ class ViewController: UIViewController {
     @IBAction func primeButtonTapped(_ sender: Any)  {
         if let num = Int(enterNum.text!)
         {
-            var divArr: [Int] = []
-            for var i in 2..<num {
-                if(num % i == 0) {
-                    divArr.append(i)
+            if (num != 0 && num != 1) {
+                var divArr: [Int] = []
+                for var i in 2..<num {
+                    if(num % i == 0) {
+                        divArr.append(i)
+                    }
                 }
-            }
-            if divArr.isEmpty {
-                primeMessage.text = "Yes, \(num) is a prime number"
+                if divArr.isEmpty && num != 1 {
+                    primeMessage.text = "Yes, \(num) is a prime number"
+                } else {
+                    primeMessage.text = "No, " + String(num) + " is NOT prime. \n It can be divided by " + String(divArr[0])
+                }
             } else {
-                primeMessage.text = "No, " + String(num) + " is NOT prime. \n It can be divided by " + String(divArr[0])
+            primeMessage.text = "Please do not enter 0 or 1"
             }
+
         } else {
-            primeMessage.text = "Please enter a non-negative whole number"
+            primeMessage.text = "Please enter a whole non-negative number"
         }
     }
     
