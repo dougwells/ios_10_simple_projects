6b07a08c31c9abd648003bf464905492ec2fd93c
Doug Wells <dougwells@gmail.com>
2016-11-23 08:54:48 -0700

    iOS. Simple To Do. Add table.

--- simpleToDo/simpleToDo/FirstViewController.swift
+++ simpleToDo/simpleToDo/FirstViewController.swift
@@ -8,7 +8,21 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let arr = ["Savvy", "Sierra", "Linda", "Doug"]
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arr.count
+    }
+
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = arr[indexPath.row]
+        
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
