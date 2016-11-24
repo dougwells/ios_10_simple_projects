df683d19602b666cdfa7e8d84541555675868916
Doug Wells <dougwells@gmail.com>
2016-11-24 07:52:39 -0700

    iOS. Simple ToDo App. Add textField behavior

--- simpleToDo/simpleToDo/SecondViewController.swift
+++ simpleToDo/simpleToDo/SecondViewController.swift
@@ -8,16 +8,34 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var newToDo: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func addNewToDoTapped(_ sender: Any) {
+        
+        newToDo.text = ""
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 
 
