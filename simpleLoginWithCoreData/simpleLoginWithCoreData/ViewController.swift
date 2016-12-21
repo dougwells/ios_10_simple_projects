73cf611c8b53b81f86be2b4ef5d7969a0407103a
Doug Wells <dougwells@gmail.com>
2016-12-21 08:10:16 -0700

    ios. Simple Login. Add save new user to Core Data.

--- simpleLoginWithCoreData/simpleLoginWithCoreData/ViewController.swift
+++ simpleLoginWithCoreData/simpleLoginWithCoreData/ViewController.swift
@@ -11,13 +11,17 @@ import CoreData
 
 class ViewController: UIViewController {
 
-    let isUserLoggedIn = false
+    var existingUser = false
+    var isUserLoggedIn = false
+    var username = ""
     @IBOutlet weak var welcomeLabel: UILabel!
     @IBOutlet weak var inputUserName: UITextField!
     
     @IBAction func login(_ sender: Any) {
         if let newUsername = inputUserName.text as String? {
             self.welcomeLabel.text = "Welcome: \(newUsername)"
+            self.inputUserName.text = ""
+            username = newUsername
         }
     }
 
@@ -35,10 +39,26 @@ class ViewController: UIViewController {
         
         //create object "newUser" that lets us save new data
         let newUser = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+
+        print("Pre. existingUser = ", existingUser)
+        checkIfExistingUser(context: context)
+        print("Post. existingUser = ", existingUser)
         
-        if !self.isUserLoggedIn {
-            
+        //save username
+        if !existingUser {
+            newUser.setValue(username, forKey: "username")
+            do {
+                try context.save()
+                print("New user, \(username), saved.")
+                self.welcomeLabel.text = "Welcome New User: \(username)"
+            } catch {
+                print("Error saving new username in do try")
+            }
+        } else {
+            self.welcomeLabel.text = "Welcome Back, \(username) !"
         }
+        
+
         
         
 
@@ -47,6 +67,33 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func checkIfExistingUser(context: NSManagedObjectContext){
+        //Check to see if this user already exists
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            
+            let results = try context.fetch(request)
+            
+            if (results.count > 0) {
+                
+                //loop thru array of result objects to see if username exists already
+                for result in results as! [NSManagedObject] {
+                    if let existingUsername = result.value(forKey: "username") as? String {
+                        
+                        if username == existingUsername {
+                            existingUser = true
+                        }
+                    }
+                }
+            }
+        } catch {
+            print("Error retrieving user data")
+        }
     }
 
 
