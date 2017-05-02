f49082004b3c588b85d0fc1ff7d5671a9efe410c
dougwells <dougwells@gmail.com>
2017-05-02 07:41:11 -0600

    Simple Practice. ScrollView. Place 3 images on 3 different "pages" of scroll view.

--- PageTheScroll/PageTheScroll/ViewController.swift
+++ PageTheScroll/PageTheScroll/ViewController.swift
@@ -11,16 +11,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    
     var images = [UIImageView]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        var contentWidth: CGFloat = 0.0
+        
         for x in 0...2 {
             let image = UIImage(named: "icon\(x)")
-            images.append(UIImageView(image: image))
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat
+            newX = view.frame.width * (0.5 + CGFloat(x))
+            
+            contentWidth += newX
+            
+            scrollView.addSubview(imageView)
+            imageView.frame = CGRect(x: newX - 75, y: view.frame.midY - 75, width: 150, height: 150)
         }
-        print("Count: \(images.count)")
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
 
     }
 
