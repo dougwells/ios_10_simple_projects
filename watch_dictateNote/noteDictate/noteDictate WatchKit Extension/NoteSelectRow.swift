d4dfc7896367ee4d11cecea751006271cc528d29
dougwells <dougwells@gmail.com>
2017-06-01 19:20:41 -0600

    Watch. noteDictator. Add first rows.

---
+++ watch_dictateNote/noteDictate/noteDictate WatchKit Extension/NoteSelectRow.swift
@@ -0,0 +1,15 @@
+//
+//  NoteSelectRow.swift
+//  noteDictate
+//
+//  Created by Doug Wells on 5/29/17.
+//  Copyright © 2017 Doug Wells. All rights reserved.
+//
+
+import WatchKit
+
+class NoteSelectRow: NSObject {
+
+    @IBOutlet var textLabel: WKInterfaceLabel!
+    
+}
